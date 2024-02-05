216881a49c3229e1a9e9a994b5d7dfe5db0453bd
say-sae <saeed.sayegh@gmail.com>
2024-02-04 21:16:12 -0500

    modified:   initialevalscript.js

--- initialevalscript.js
+++ initialevalscript.js
@@ -1,10 +1,13 @@
 function generatePDF() {
-    const {jsPDF} = window.jspdf;
+    const { jsPDF } = window.jspdf;
     const doc = new jsPDF();
 
     // VARIABLES
 
-            //SUBJECTIVE VARIABLES
+    var verticalposition = 10;
+    
+
+    //SUBJECTIVE VARIABLES
     var subloc = document.getElementById('location').value;
     var subons = document.getElementById('onset').value;
     var submoi = document.getElementById('moi').value;
@@ -15,34 +18,37 @@ function generatePDF() {
     var subocc = document.getElementById('occupation').value;
     var subtext = document.getElementById('subjectivetext').value;
 
-            //OBJECTIVE VARIABLES
+    //OBJECTIVE VARIABLES
 
-                //SHOULDER VARIABLEs
-            var shoulderobservation = document.getElementById('shoulderobservation').value;
-            var lshouldermmtflex = document.getElementById('lshouldermmtflex').value;
-            var lshouldermmtplusminusflex = document.getElementById('lshouldermmtplusminusflex').value;
-            var lshouldermmtext = document.getElementById('lshouldermmtext').value;
-            var lshouldermmtplusminusext = document.getElementById('lshouldermmtplusminusext').value;
-            var lshouldermmtabd = document.getElementById('lshouldermmtabd').value;
-            var lshouldermmtplusminusabd = document.getElementById('lshouldermmtplusminusabd').value;
-            var lshouldermmter = document.getElementById('lshouldermmter').value;
-            var lshouldermmtplusminuser = document.getElementById('lshouldermmtplusminuser').value;
-            var lshouldermmtir = document.getElementById('lshouldermmtir').value;
-            var lshouldermmtplusminusir = document.getElementById('lshouldermmtplusminusir').value;
 
-            var lshoulderaromflex = document.getElementById('lshoulderaromflex').value;
-            var lshoulderaromext = document.getElementById('lshoulderaromext').value;
-            var lshoulderaromabd = document.getElementById('lshoulderaromabd').value;
-            var lshoulderaromer = document.getElementById('lshoulderaromer').value;
-            var lshoulderaromir = document.getElementById('lshoulderaromir').value;
 
-            var lshoulderpromflex = document.getElementById('lshoulderpromflex').value;
-            var lshoulderpromext = document.getElementById('lshoulderpromext').value;
-            var lshoulderpromabd = document.getElementById('lshoulderpromabd').value;
-            var lshoulderpromer = document.getElementById('lshoulderpromer').value;
-            var lshoulderpromir = document.getElementById('lshoulderpromir').value;
-            
-          
+
+    //SHOULDER VARIABLEs
+    var shoulderobservation = document.getElementById('shoulderobservation').value;
+    var lshouldermmtflex = document.getElementById('lshouldermmtflex').value;
+    var lshouldermmtplusminusflex = document.getElementById('lshouldermmtplusminusflex').value;
+    var lshouldermmtext = document.getElementById('lshouldermmtext').value;
+    var lshouldermmtplusminusext = document.getElementById('lshouldermmtplusminusext').value;
+    var lshouldermmtabd = document.getElementById('lshouldermmtabd').value;
+    var lshouldermmtplusminusabd = document.getElementById('lshouldermmtplusminusabd').value;
+    var lshouldermmter = document.getElementById('lshouldermmter').value;
+    var lshouldermmtplusminuser = document.getElementById('lshouldermmtplusminuser').value;
+    var lshouldermmtir = document.getElementById('lshouldermmtir').value;
+    var lshouldermmtplusminusir = document.getElementById('lshouldermmtplusminusir').value;
+
+    var lshoulderaromflex = document.getElementById('lshoulderaromflex').value;
+    var lshoulderaromext = document.getElementById('lshoulderaromext').value;
+    var lshoulderaromabd = document.getElementById('lshoulderaromabd').value;
+    var lshoulderaromer = document.getElementById('lshoulderaromer').value;
+    var lshoulderaromir = document.getElementById('lshoulderaromir').value;
+
+    var lshoulderpromflex = document.getElementById('lshoulderpromflex').value;
+    var lshoulderpromext = document.getElementById('lshoulderpromext').value;
+    var lshoulderpromabd = document.getElementById('lshoulderpromabd').value;
+    var lshoulderpromer = document.getElementById('lshoulderpromer').value;
+    var lshoulderpromir = document.getElementById('lshoulderpromir').value;
+
+
 
     var tableData = [
         ["Location", subloc],
@@ -55,27 +61,82 @@ function generatePDF() {
         ["Occupation", subocc]
     ];
 
-            //ASSESSMENT VARIABLES
+    //ASSESSMENT VARIABLES
 
 
 
-            //PLAN VARIABLES
-   
+    //PLAN VARIABLES
 
-   //styling
 
-        //SUB STYLE
-    doc.text("Location: " + subloc, 20, 20);
-    doc.text("Onset: " + subons, 20, 30);
-    doc.text("Mechanism of Injury: " + submoi, 20, 40);
-    doc.text("Aggravating Factors: " + subagg, 20, 50);
-    doc.text("Easing Factors: " + subeas, 20, 60);
-    doc.text("Imaging: " + subimg, 20, 70);
-    doc.text("Hobbies: " + subhob, 20, 80);
-    doc.text("Occupation: " + subocc, 20, 90);
-    doc.text(subtext, 20, 100);
-    
-        //OBJ STYLE
+    //styling
+
+    //print functions
+
+    //SUB PRINT
+
+
+    function hasUserInput(value) {
+        return value.trim() !== '';
+    }
+
+    if (hasUserInput(subloc)) {
+        doc.setFont("courier", "bold");
+        doc.text("Location: ", 20, verticalposition);
+        doc.setFont("courier", "normal");
+        doc.text(subloc, 55, verticalposition);
+        verticalposition += 10;
+    }
+
+    if (hasUserInput(subons)) {
+        doc.setFont("courier", "bold");
+        doc.text("Onset: ", 20, verticalposition);
+        doc.setFont("courier", "normal");
+        doc.text(subons, 45, verticalposition);
+        verticalposition += 10;
+    }
+
+    if (hasUserInput(submoi)) {
+        doc.setFont("courier", "bold");
+        doc.text("Mecahnism of Injury: ", 20, verticalposition);
+        doc.setFont("courier", "normal");
+        doc.text(submoi, 100, verticalposition);
+        verticalposition += 10;
+    }
+
+    if (hasUserInput(subagg)) {
+        doc.text("Aggravating Factors: " + subagg, 20, verticalposition);
+        verticalposition += 10;
+    }
+
+    if (hasUserInput(subeas)) {
+        doc.text("Easing Factors: " + subeas, 20, verticalposition);
+        verticalposition += 10;
+    }
+
+    if (hasUserInput(subimg)) {
+        doc.text("Imaging: " + subimg, 20, verticalposition);
+        verticalposition += 10;
+    }
+
+    if (hasUserInput(subhob)) {
+        doc.text("Hobbies: " + subhob, 20, verticalposition);
+        verticalposition += 10;
+    }
+
+    if (hasUserInput(subocc)) {
+        doc.text("Occupation: " + subocc, 20, verticalposition);
+        verticalposition += 10;
+    }
+
+    if (hasUserInput(subtext)) {
+        doc.text(subtext, 20, verticalposition);
+        verticalposition += 10;
+    }
+
+    //OBJ STYLE
+
+
+
     doc.text("Observation: " + shoulderobservation, 20, 120);
     doc.text("L shoulder flexion MMT: " + lshouldermmtflex + lshouldermmtplusminusflex + "/5", 20, 130);
     doc.text("L shoulder extension MMT: " + lshouldermmtext + lshouldermmtplusminusext + "/5", 20, 140);
@@ -88,7 +149,8 @@ function generatePDF() {
     doc.text("L shoulder abduction AROM: " + lshoulderaromabd, 20, 200);
     doc.text("L shoulder external rotation AROM: " + lshoulderaromer, 20, 210);
     doc.text("L shoulder internal rotation AROM: " + lshoulderaromir, 20, 220);
-    
+
+
     doc.save("pteval.pdf")
 
 } 
